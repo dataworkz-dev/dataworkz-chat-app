dd476a4e560ab0b4effa31851a7606c86137e100
mayank2715937 <mayank@dataworkz.io>
2024-09-02 22:55:49 +0530

    adding enterprise base url variable

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Installs the dependencies of the project.
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+### `Changing variables to access dataworkz api`
+
+Go inside src folder and then App.js. You'll find the component `<Chatbot systemId={systemId} llmId={llmId} token={token} baseUrl={baseUrl} enterpriseSearchId={enterpriseSearchId} enterpriseSearchToken={enterpriseSearchToken} enterpriseBaseUrl={enterpriseBaseUrl}/>`.
+You can change the variables as per your requirement.
--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const llmId="99ecafe6-1b0b-4333-b670-ebc84458fc1b";
 const baseUrl = 'https://genai.dataworkz.com/api/qna/v1/systems/';
 const enterpriseSearchId = "1e459343-a593-44fd-bb7d-54aeea0659e2";
 const enterpriseSearchToken = 'eyJhbGciOiJSUzI1NiJ9.eyJwcml2cyI6WyJhZG1pbiJdLCJmdW5jcyI6W10sInN1YiI6InNhY2hpbi5oZWppcEBkYXRhd29ya3ouaW8iLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjgwOTAvZGF0YXdvcmt6L3ZlcmlmeS9hcGlrZXkiLCJpc3MiOiJzZXJ2aWNlQWNjb3VudEBkYXRhd29ya3ouaW8iLCJpYXQiOjE3MjUyNTYzNzN9.fPJvMbWOHG_dzML-Q-Xd5Vxc-IceNmJsB3qtHp6t2Awkul1t5_Vao2NNvJzx2UZul-QonQTyjYts62kecWClmSpUo3P5iVPiC4mZeqIecvDHUoy5pfVNMJ8tAaiV4YJ3MoBeMdv3T6qN8ZF8c5aTElckpHVN_VAyjYQzYUiX09NWxTMkchYndOTwx0s9gnk_lyHysPzx2RAoDLxAq7e4Ou_xxrWT96qCi-ERS6XgoKPMMDUTihTpYcvjwSyJwXC-Zql-roEeB7FVxok8XnoZRUTlDW2cfE7vol6oxdNydZy5JH68MqDsVtsCnWuTFMCnQEqHqo3ZpD1VwMIrKL_Wpg'
+const enterpriseBaseUrl = 'https://genai.dataworkz.com/api/qna/v1/systems/';
 const App = () => (
   
   <div>
@@ -27,7 +28,8 @@ const App = () => (
           apps without the need to manage infrastructure or learn new developer skills.
         </div>
         <div className='button-div'>
-          <Chatbot systemId={systemId} llmId={llmId} token={token} baseUrl={baseUrl} enterpriseSearchId={enterpriseSearchId} enterpriseSearchToken={enterpriseSearchToken}/>
+          <Chatbot systemId={systemId} llmId={llmId} token={token} baseUrl={baseUrl} enterpriseSearchId={enterpriseSearchId} enterpriseSearchToken={enterpriseSearchToken}
+           enterpriseBaseUrl={enterpriseBaseUrl}/>
         </div>
       </div>
     </div>
